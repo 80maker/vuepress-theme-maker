ba93d4808fdbc9792df86e68e7189e3f36b6013e
80maker <cmgddd@126.com>
2020-11-27 17:57:01 +0800

    up

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: '1980\'s Maker',
-  description: '一个出生于80年代的程序员、创[客]、美[食]、电[影]、读[书]、怀[旧]的新手艺人',
+  description: '一个出生于80年代的程序员、创[客]、美[食]、动[画]、读[书]、怀[旧]的新手艺人',
   markdown: {
     lineNumbers: true
   },
@@ -9,7 +9,7 @@ module.exports = {
       { text: '程序员', link: '/frontend/' },
       { text: '创(客)', link: '/maker/' },
       { text: '美(食)', link: '/foods/' },
-      { text: '电(影)', link: '/movie/' },
+      { text: '动(画)', link: '/movie/' },
       { text: '读(书)', link: '/books/' },
       { text: '旧时光', link: '/old-time/' },
     ]
